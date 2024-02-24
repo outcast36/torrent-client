5008c8b8499ed0327dcea621125d4c044129cb30
Wyatt Ayers <ayersw2@wwu.edu>
2024-02-23 16:49:25 -0800

    Experimenting with JSON for decoding return types

--- src/Parser.h
+++ src/Parser.h
@@ -1,6 +1,9 @@
 #include <string>
 #include <cstdint>
-#include <unordered_map>
+#include <vector>
+#include "lib/nlohmann/json.hpp"
+
+using json = nlohmann::json;
 
 namespace BitTorrent {
     class Parser {
@@ -8,8 +11,11 @@ namespace BitTorrent {
         Parser(std::string metainfo);
         std::string getMetainfo();
         bool isTorrentFile(std::string metainfo);
+        std::vector<std::string> tokenize(std::string encoded);
+        json decodeString(std::string encoded);
         std::int64_t decodeInt(std::string encoded);
-        std::string decodeString(std::string encoded);
+        std::vector<std::string> decodeList(std::string encoded);
+        // map<string,string> decodeDict(std::string encoded);
     private:
         std::string torrent;
     };
