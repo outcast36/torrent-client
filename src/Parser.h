7eff26938741762248a4322113c6d9ed9f3a67b6
Wyatt Ayers <ayersw2@wwu.edu>
2024-03-31 17:46:28 -0700

    Made some decoding tools private

--- src/Parser.h
+++ src/Parser.h
@@ -9,13 +9,11 @@ namespace BitTorrent {
     class Parser {
     public:
         Parser(std::string metainfo);
-        std::string getMetainfo();
-        bool isTorrentFile(std::string metainfo);
-        std::vector<std::string> tokenize(std::string encoded);
-        json decode(std::vector<std::string>& tokens, int64_t& idx);
         void decodeFile(json& decoded, std::string& info);
     private:
         std::string torrent;
+        std::vector<std::string> tokenize(std::string encoded);
+        json decodeTokens(std::vector<std::string>& tokens, int64_t& idx);
     };
 }
 
